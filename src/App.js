245606b207c215acc25c3b6cf84f8c0996cd0b10
Blair Anderson <blair81@gmail.com>
2019-02-22 15:14:14 -0800

    add a link in the header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from "react";
 import Cleaner from "./Cleaner";
 
-const textCenter = { textAlign: "center" };
+const textCenter = { clear: "both", textAlign: "center" };
 
 class App extends Component {
   render() {
     return (
-      <div style={textCenter}>
-        <h1>TEXT to ASINs</h1>
-        <h3>Paste Text on the LEFT - ASINs output on the right</h3>
-        <Cleaner />
+      <div>
+        <h1 style={textCenter}>
+          <a href="https://getasins.netlify.com/">TEXT to ASINs</a>
+        </h1>
+        <h3 style={textCenter}>
+          Paste Text on the LEFT - ASINs output on the right
+        </h3>
         <div>
+          <Cleaner />
+        </div>
+        <div style={textCenter}>
+          A tiny script built by{" "}
+          <a href="https://www.andersonassociates.net">Blair Anderson</a>.
+          <br />
           <small>
-            <a href="https://codesandbox.io/s/github/blairanderson/TextToASIN">
-              fork on codesandbox
+            <a
+              rel="nofollow noreferer"
+              href="https://github.com/blairanderson/TextToASIN/blob/master/README.md"
+            >
+              100% Open Source
             </a>
           </small>
         </div>
