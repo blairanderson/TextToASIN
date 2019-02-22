d9fb5778aeb02946dbe87ba6658599d990051a34
Blair Anderson <blair81@gmail.com>
2019-02-22 11:52:14 -0800

    update the footer link

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 import Cleaner from "./Cleaner";
 
-const textCenter = { textAlign: "center" };
+const textCenter = { clear: "both", textAlign: "center" };
 
 class App extends Component {
   render() {
     return (
-      <div style={textCenter}>
-        <h1>TEXT to ASINs</h1>
-        <h3>Paste Text on the LEFT - ASINs output on the right</h3>
-        <Cleaner />
+      <div>
+        <h1 style={textCenter}>TEXT to ASINs</h1>
+        <h3 style={textCenter}>
+          Paste Text on the LEFT - ASINs output on the right
+        </h3>
         <div>
+          <Cleaner />
+        </div>
+        <div style={textCenter}>
           <small>
-            <a href="https://codesandbox.io/s/github/blairanderson/TextToASIN">
-              fork on codesandbox
-            </a>
+            A tiny script built by{" "}
+            <a href="https://www.andersonassociates.net">Blair Anderson</a>
           </small>
         </div>
       </div>
